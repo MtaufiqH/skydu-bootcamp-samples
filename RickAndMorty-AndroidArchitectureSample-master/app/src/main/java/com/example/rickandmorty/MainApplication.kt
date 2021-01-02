44d4a73961b9f87926a0318ba68c0998b9a9be42
cakcaraka <caraka@traveloka.com>
2021-01-03 00:51:28 +0700

    cleanup

--- RickAndMorty-AndroidArchitectureSample-master/app/src/main/java/com/example/rickandmorty/MainApplication.kt
+++ RickAndMorty-AndroidArchitectureSample-master/app/src/main/java/com/example/rickandmorty/MainApplication.kt
@@ -5,13 +5,9 @@ import timber.log.Timber
 
 class MainApplication : Application() {
 
-    companion object {
-        lateinit var application: Application
-    }
 
     override fun onCreate() {
         super.onCreate()
-        application = this
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
         }
