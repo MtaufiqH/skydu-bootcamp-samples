b430aa129b2d3857fd61661535ff1451d9cbae66
cakcaraka <caraka@traveloka.com>
2021-01-03 01:27:09 +0700

    init demo

---
+++ RickyMortyDemo/app/src/main/java/skydu/android/rickymortydemo/ui/characters/CharactersActivity.kt
@@ -0,0 +1,12 @@
+package skydu.android.rickymortydemo.ui.characters
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import skydu.android.rickymortydemo.R
+
+class CharactersActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
