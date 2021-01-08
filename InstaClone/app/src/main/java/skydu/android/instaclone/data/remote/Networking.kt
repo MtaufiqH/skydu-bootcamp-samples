4f6d58bd8745aa750bd186d37c6eabc2f8b54213
cakcaraka <caraka@traveloka.com>
2021-01-08 10:32:42 +0700

    Implement Interceptor untuk headers

--- InstaClone/app/src/main/java/skydu/android/instaclone/data/remote/Networking.kt
+++ InstaClone/app/src/main/java/skydu/android/instaclone/data/remote/Networking.kt
@@ -6,6 +6,7 @@ import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import skydu.android.instaclone.BuildConfig
+import java.util.concurrent.TimeUnit
 
 
 object Networking {
@@ -13,8 +14,27 @@ object Networking {
     val service: NetworkService by lazy {
         Retrofit.Builder()
             .baseUrl("https://skydu-insta-api.herokuapp.com")
+            .client(
+                OkHttpClient.Builder()
+                    .addInterceptor(
+                        HttpLoggingInterceptor()
+                        .apply {
+                            level = if (BuildConfig.DEBUG) HttpLoggingInterceptor.Level.BODY
+                            else HttpLoggingInterceptor.Level.NONE
+                        })
+                    .addInterceptor {
+                        val newRequest: Request = it.request().newBuilder()
+                            .addHeader("Accept", "application/json")
+                            .build()
+                        it.proceed(newRequest)
+                    }
+                    .readTimeout(60.toLong(), TimeUnit.SECONDS)
+                    .writeTimeout(60.toLong(), TimeUnit.SECONDS)
+                    .build()
+            )
             .addConverterFactory(GsonConverterFactory.create())
             .build()
+
             .create(NetworkService::class.java)
     }
 
