a35bd749e9c48d91a99120fadf7baf9c3f77db53
cakcaraka <caraka@traveloka.com>
2021-01-08 13:41:42 +0700

    rebase timeutils

--- InstaClone/app/src/main/java/skydu/android/instaclone/utils/TimeUtils.kt
+++ InstaClone/app/src/main/java/skydu/android/instaclone/utils/TimeUtils.kt
@@ -4,22 +4,15 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 object TimeUtils {
-
-    fun getTime(date: Long): String {
-
-        val format = SimpleDateFormat("yyyy-MM-dd hh:mm")
-
-        val dateString: String = format.format(date)
-
-        return dateString
-    }
-
-    fun getTimeInMillis(date: String): Long {
+    
+    fun convertTimeFormat(date: String): String {
         val dateFormat = SimpleDateFormat(
             "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'",
             Locale.getDefault()
         )
         dateFormat.timeZone = TimeZone.getTimeZone("GMT")
-        return dateFormat.parse(date).time
+        val millis = dateFormat.parse(date).time
+
+        return SimpleDateFormat("yyyy-MM-dd hh:mm", Locale.getDefault()).format(millis)
     }
 }
