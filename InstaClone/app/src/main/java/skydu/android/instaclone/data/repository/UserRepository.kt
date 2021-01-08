18b78f1a1beafeaa57f3547a6f0fd519474d143c
cakcaraka <caraka@traveloka.com>
2021-01-08 11:00:41 +0700

    Implement save username dan token

--- InstaClone/app/src/main/java/skydu/android/instaclone/data/repository/UserRepository.kt
+++ InstaClone/app/src/main/java/skydu/android/instaclone/data/repository/UserRepository.kt
@@ -3,13 +3,16 @@ package skydu.android.instaclone.data.repository
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.liveData
 import kotlinx.coroutines.Dispatchers
+import skydu.android.instaclone.data.local.prefs.UserPreferences
 import skydu.android.instaclone.data.remote.NetworkService
 import skydu.android.instaclone.data.remote.Networking
 import skydu.android.instaclone.data.remote.login.LoginRequest
+import skydu.android.instaclone.data.remote.login.LoginResponse
 import skydu.android.instaclone.data.repository.model.DataResult
 
 class UserRepository {
     private val networkService: NetworkService = Networking.service
+    private val userPreferences = UserPreferences()
 
     fun doUserLogin(username: String, password: String): LiveData<DataResult<Unit>> =
         liveData(Dispatchers.IO) {
@@ -24,7 +27,7 @@ class UserRepository {
 
             if (result.state == DataResult.State.SUCCESS) {
                 result.data?.run {
-                    saveCurrentUser(this.profile, this.token)
+                    saveCurrentUser(this)
                     emit(
                         DataResult(
                             DataResult.State.SUCCESS, Unit, null
@@ -35,4 +38,10 @@ class UserRepository {
                 emit(DataResult<Unit>(result.state, null, result.errorMessage))
             }
         }
+
+
+    private fun saveCurrentUser(loginReponse: LoginResponse) {
+        userPreferences.setUserName(loginReponse.profile.username)
+        userPreferences.setAccessToken(loginReponse.token)
+    }
 }
